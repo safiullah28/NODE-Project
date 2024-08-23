32d5d24471577192a00677609ba7df0d718d0f01
safiullah28 <safiullah424@gmail.com>
2024-08-23 22:51:30 +0500

    Complete Project

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+require("dotenv").config();
+const Product = require("./models/productModel");
 app.use(bodyParser.urlencoded({ extended: true }));
-const port = 3000;
-const uri =
-    "mongodb+srv://MERN:MERN@cluster0.99ai1.mongodb.net/NODE-API?retryWrites=true&w=majority&appName=Cluster0";
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+const port = process.env.PORT || 3000;
+
+const uri = process.env.URI;
 
 mongoose.set("strictQuery", false);
 mongoose
@@ -23,7 +27,68 @@ mongoose
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
-app.get("/blog", (req, res) => {
-    res.send("Hello Blog!");
-    console.log("GET request on blog");
+
+app.post("/products", async(req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(201).json(product);
+    } catch (error) {
+        console.error("Error creating product", error.message);
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get("/products", async(req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json({ products });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.get("/products/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.put("/products/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if (!product) {
+            //Product not found in the database
+            return res
+                .status(404)
+                .json({ message: "Product with id " + id + " not found" });
+        }
+        //Product updated successfully
+        const foundedProduct = await Product.findById(id);
+        res.status(200).json(foundedProduct);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+app.delete("/products/:id", async(req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findByIdAndDelete(id, req.body);
+        if (!product) {
+            //Product not found in the database
+            return res
+                .status(404)
+                .json({ message: "Product with id " + id + " not found" });
+        }
+        //Product updated successfully
+        const foundedProduct = await Product.findById(id);
+        res.status(200).json(foundedProduct);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
---
+++ models/productModel.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const productSchema = mongoose.Schema({
+    name: { type: String, required: [true, "Please enter a product name"] },
+    quantity: { type: Number, required: true, default: "" },
+    price: { type: Number, required: true },
+    imageURL: { type: String, required: false },
+}, {
+    timestamps: true,
+});
+
+const Product = mongoose.model("Product", productSchema);
+
+module.exports = Product;
